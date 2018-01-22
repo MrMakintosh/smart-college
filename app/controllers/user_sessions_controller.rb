8b6cc57aba6c5cc9f7fb1973d745e8d9a0a7c837
Alexander <chaplin1488@gmail.com>
2018-01-22 10:30:26 +0300

    Styles finished at all. Maybe repair bugs later and in another brunch

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(user_session_params)
     if @user_session.save
-      flash[:success] = "Welcome back!"
+      flash[:success] = "Добро пожаловать!"
       redirect_to root_path
     else
       flash[:alert] = "Неправильный логин или пароль!"
@@ -26,6 +26,5 @@ class UserSessionsController < ApplicationController
   def user_session_params
     params.require(:user_session).permit(:login, :password, :remember_me)
   end
-##TODO: commit "mathematics for user sign_in/sign_out was done. Need styles and front-end at all."
 ##TODO: need passes realization for students and graphs showed in admin menu (make profile menu different for user and admin)
 end
